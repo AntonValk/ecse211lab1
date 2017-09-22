c31bf277dd1fe95c78e64697538dfd8ca0780ecf
Antonios Valkanas <30447660+AntonValk@users.noreply.github.com>
2017-09-22 12:01:50 -0400

    some lab2
    i upload here because i cant upload on the new repo yet

---
+++ src/OdometryDisplay.java
@@ -0,0 +1,102 @@
+import lejos.hardware.lcd.TextLCD;
+
+public class OdometryDisplay extends Thread {
+	private static final long DISPLAY_PERIOD = 250;
+	private Odometer odometer;
+	private TextLCD t;
+	float color[];
+
+	// constructor
+	public OdometryDisplay(Odometer odometer, TextLCD t) {
+		this.odometer = odometer;
+		this.t = t;
+		color = new float[Lab2.colorSensor.sampleSize()];
+		color[0] = 0.0f;
+	}
+
+	// run method (required for Thread)
+	public void run() {
+		long displayStart, displayEnd;
+		double[] position = new double[3];
+
+		// clear the display once
+		t.clear();
+
+		while (true) {
+			displayStart = System.currentTimeMillis();
+			Lab2.colorSensor.fetchSample(color,0);
+			color[0] *= 1000.0f;
+
+			// clear the lines for displaying odometry information
+			t.drawString("X:              ", 0, 0);
+			t.drawString("Y:              ", 0, 1);
+			t.drawString("T:              ", 0, 2);
+			//t.drawString("Color: " + Lab2.lightSensor.getColorID(), 0, 3);
+			//t.drawString("Color: [" + color[0] + ", " + color[1] + ", " + color[2] + "]",0,3);
+			t.drawString("" + color[0],0,3); //Display intensity for debugging
+			t.drawString("Count: " + OdometryCorrection.count,0,4); //Display line count for debugging
+			
+			// get the odometry information
+			odometer.getPosition(position, new boolean[] { true, true, true });
+
+			// display odometry information
+			for (int i = 0; i < 3; i++) {
+				t.drawString(formattedDoubleToString(position[i], 2), 3, i);
+			}
+
+			// throttle the OdometryDisplay
+			displayEnd = System.currentTimeMillis();
+			if (displayEnd - displayStart < DISPLAY_PERIOD) {
+				try {
+					Thread.sleep(DISPLAY_PERIOD - (displayEnd - displayStart));
+				} catch (InterruptedException e) {
+					// there is nothing to be done here because it is not
+					// expected that OdometryDisplay will be interrupted
+					// by another thread
+				}
+			}
+		}
+	}
+	
+	private static String formattedDoubleToString(double x, int places) {
+		String result = "";
+		String stack = "";
+		long t;
+		
+		// put in a minus sign as needed
+		if (x < 0.0)
+			result += "-";
+		
+		// put in a leading 0
+		if (-1.0 < x && x < 1.0)
+			result += "0";
+		else {
+			t = (long)x;
+			if (t < 0)
+				t = -t;
+			
+			while (t > 0) {
+				stack = Long.toString(t % 10) + stack;
+				t /= 10;
+			}
+			
+			result += stack;
+		}
+		
+		// put the decimal, if needed
+		if (places > 0) {
+			result += ".";
+		
+			// put the appropriate number of decimals
+			for (int i = 0; i < places; i++) {
+				x = Math.abs(x);
+				x = x - Math.floor(x);
+				x *= 10.0;
+				result += Long.toString((long)x);
+			}
+		}
+		
+		return result;
+	}
+
+}
